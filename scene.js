65a7724b73ea77edabf3c69acaebb594ba914dab
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2013-11-04 03:27:43 +0200

    object build

--- scene.js
+++ scene.js
@@ -8,13 +8,19 @@ animate();
 
 function init()
 {
+
+  // SCENE
   scene = new THREE.Scene();
-  var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight;	
+
+  // CAMERA
+  var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight;
   var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 20000;
   camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
   scene.add(camera);
-  camera.position.set(0,15,4);
+  camera.position.set(0,150,400);
   camera.lookAt(scene.position);
+
+  // RENDERER
   if ( Detector.webgl )
     renderer = new THREE.WebGLRenderer( {antialias:true} );
   else
@@ -22,89 +28,75 @@ function init()
   renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
   container = document.getElementById( 'ThreeJS' );
   container.appendChild( renderer.domElement );
+
+  // EVENTS
   THREEx.WindowResize(renderer, camera);
   THREEx.FullScreen.bindKey({ charCode : 'm'.charCodeAt(0) });
+
+  // CONTROLS
   controls = new THREE.OrbitControls( camera, renderer.domElement );
 
+  // STATS
   stats = new Stats();
   stats.domElement.style.position = 'absolute';
   stats.domElement.style.bottom = '0px';
   stats.domElement.style.zIndex = 100;
   container.appendChild( stats.domElement );
 
+  // LIGHT
   var light = new THREE.PointLight(0xffffff);
-  light.position.set(0,10,0);
+  light.position.set(100,250,100);
   scene.add(light);
 
-  var light = new THREE.PointLight(0xffffff);
-  light.position.set(0,-10,0);
-  scene.add(light);
+  // FLOOR
+  var floorTexture = new THREE.ImageUtils.loadTexture( 'images/checkerboard.jpg' );
+  floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
+  floorTexture.repeat.set( 10, 10 );
+  var floorMaterial = new THREE.MeshBasicMaterial( { map: floorTexture, side: THREE.DoubleSide } );
+  var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);
+  var floor = new THREE.Mesh(floorGeometry, floorMaterial);
+  floor.position.y = -0.5;
+  floor.rotation.x = Math.PI / 2;
+  scene.add(floor);
 
-  var light = new THREE.PointLight(0xffffff);
-  light.position.set(10,0,0);
-  scene.add(light);
-
-  var light = new THREE.PointLight(0xffffff);
-  light.position.set(-10,0,0);
-  scene.add(light);
-
-  var light = new THREE.PointLight(0xffffff);
-  light.position.set(0,0,10);
-  scene.add(light);
-
-  var light = new THREE.PointLight(0xffffff);
-  light.position.set(0,0,-10);
-  scene.add(light);
-
-  var ambientLight = new THREE.AmbientLight(0x111111);
-  scene.add(ambientLight);
-
-  //var sphereGeometry = new THREE.SphereGeometry( 50, 32, 16 );
-  //var sphereMaterial = new THREE.MeshLambertMaterial( {color: 0x8888ff} );
-  //var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-  //sphere.position.set(100, 50, -50);
-  //scene.add(sphere);
-
-  var hGeometry = new THREE.SphereGeometry( 1.1, 50, 50 );
-  var hMaterial = new THREE.MeshLambertMaterial( {color: 0xff0000} );
-  var h = new THREE.Mesh(hGeometry, hMaterial);
-  h.position.set(0, 0, 0);
-  scene.add(h);
-
-  var oGeometry = new THREE.SphereGeometry( 0.9, 50, 50 );
-  var oMaterial = new THREE.MeshLambertMaterial( {color: 0xffffff} );
-  var o = new THREE.Mesh(oGeometry, oMaterial);
-  o.position.set(1, 1, 0);
-  scene.add(o);
-
-  var oGeometry = new THREE.SphereGeometry( 0.9, 50, 50 );
-  var oMaterial = new THREE.MeshLambertMaterial( {color: 0xffffff} );
-  var o = new THREE.Mesh(oGeometry, oMaterial);
-  o.position.set(-1, 1, 0);
-  scene.add(o);
-
-
-  //var cubeMaterialArray = [];
-  //cubeMaterialArray.push( new THREE.MeshBasicMaterial( { color: 0xff3333 } ) );
-  //cubeMaterialArray.push( new THREE.MeshBasicMaterial( { color: 0xff8800 } ) );
-  //cubeMaterialArray.push( new THREE.MeshBasicMaterial( { color: 0xffff33 } ) );
-  //cubeMaterialArray.push( new THREE.MeshBasicMaterial( { color: 0x33ff33 } ) );
-  //cubeMaterialArray.push( new THREE.MeshBasicMaterial( { color: 0x3333ff } ) );
-  //cubeMaterialArray.push( new THREE.MeshBasicMaterial( { color: 0x8833ff } ) );
-  //var cubeMaterials = new THREE.MeshFaceMaterial( cubeMaterialArray );
-  //var cubeGeometry = new THREE.CubeGeometry( 100, 100, 100, 1, 1, 1 );
-  //cube = new THREE.Mesh( cubeGeometry, cubeMaterials );
-  //cube.position.set(-100, 50, -50);
-  //scene.add( cube );
-
-  //var axes = new THREE.AxisHelper(100);
-  //scene.add( axes );
-
+  // SKYBOX
   var skyBoxGeometry = new THREE.CubeGeometry( 10000, 10000, 10000 );
   var skyBoxMaterial = new THREE.MeshBasicMaterial( { color: 0x9999ff, side: THREE.BackSide } );
   var skyBox = new THREE.Mesh( skyBoxGeometry, skyBoxMaterial );
-  //scene.add(skyBox);
-  scene.fog = new THREE.FogExp2( 0x9999ff, 0.00025 );
+  scene.add(skyBox);
+
+  ////////////
+  // CUSTOM //
+  ////////////
+
+  Object.build = function (formula, angle, distance, coords) {
+    this.formula = formula
+    this.angle = angle
+    this.distance = distance
+    this.coords = coords
+    var molecule = [20, 30, 40];
+    molecule.forEach(function(entry) {
+      var geometry = new THREE.SphereGeometry( 20, 32, 16 );
+      var material = new THREE.MeshLambertMaterial( { color: 0xff0000 } );
+      var oxygen = new THREE.Mesh(geometry, material);
+      oxygen.position.set(0, 70, 0);
+      scene.add(oxygen);
+    });
+  };
+
+  var water = new Object.build("H-O-H", 104.45, 0.9584, [0,70,0])
+
+  //var oxygen_geometry = new THREE.SphereGeometry( 20, 32, 16 );
+  //var oxygen_material = new THREE.MeshLambertMaterial( { color: 0xff0000 } );
+  //var oxygen = new THREE.Mesh(oxygen_geometry, oxygen_material);
+  //oxygen.position.set(0, 70, 0);
+  //scene.add(oxygen);
+
+  //var hydrogen_material = new THREE.MeshLambertMaterial( { color: 0xffffff } );
+  //var hydrogen_geometry = new THREE.SphereGeometry( 15, 32, 16 );
+  //var hydrogen = new THREE.Mesh(hydrogen_geometry, hydrogen_material);
+  //hydrogen.position.set(10, 30, 0);
+  //scene.add(hydrogen);
 }
 
 function animate()
