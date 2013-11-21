3ea51412a8f18a9c443bf598b7bb822099349738
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2013-11-21 03:55:50 +0200

    carbon monoxid

--- collection.js
+++ collection.js
@@ -1,9 +1,9 @@
 atom = function(vector, element) {
-  var radius_k = 3.87;
-  if (element == "h") { color = 0xff5533; radius = 31*radius_k }
-  if (element == "o") { color = 0x0099ff; radius = 48*radius_k }
-  if (element == "c") { color = 0xffdd44; radius = 67*radius_k }
-  if (element == "n") { color = 0x44ff44; radius = 56*radius_k }
+  var radiusK = 3.87;
+  if (element == "h") { color = 0xff5533; radius = 31*radiusK }
+  if (element == "o") { color = 0x0099ff; radius = 48*radiusK }
+  if (element == "c") { color = 0xffdd44; radius = 67*radiusK }
+  if (element == "n") { color = 0x44ff44; radius = 56*radiusK }
 
   var geometry = new THREE.SphereGeometry(radius/10,32,16);
   var material = new THREE.MeshPhongMaterial({ color: color });
@@ -74,7 +74,18 @@ hydrogen = function(x, y, z) {
   scene.add( molecule );
 }
 
+carbonMonoxide = function(x, y, z) {
+  molecule = new THREE.Object3D();
+  var startPosition = new THREE.Vector3( 0, 0, 0 );
+  molecule.add(atom(startPosition, "c"));
+  var secondPosition = new THREE.Vector3( 0, 22, 0 );
+  molecule.add(atom(secondPosition, "o"));
+  molecule.position.set(x, y, z);
+  scene.add( molecule );
+}
+
 water(-150,40,0);
 methanum(-50,40,0);
 ammonia(50,40,0);
 hydrogen(130,40,0);
+carbonMonoxide(-90,40,80);
