5de329c11df62ff1f07fa173737deb0acf05e593
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2013-11-21 03:25:40 +0200

    add nh3

--- water.js
+++ water.js
@@ -1,7 +1,9 @@
 atom = function(vector, element) {
-  if (element == "h") { color = 0xff5533; radius = 120 }
-  if (element == "o") { color = 0x0099ff; radius = 155 }
-  if (element == "c") { color = 0xffdd44; radius = 170 }
+  var radius_k = 3.87;
+  if (element == "h") { color = 0xff5533; radius = 31*radius_k }
+  if (element == "o") { color = 0x0099ff; radius = 48*radius_k }
+  if (element == "c") { color = 0xffdd44; radius = 67*radius_k }
+  if (element == "n") { color = 0x44ff44; radius = 56*radius_k }
 
   var geometry = new THREE.SphereGeometry(radius/10,32,16);
   var material = new THREE.MeshPhongMaterial({ color: color });
@@ -14,12 +16,12 @@ atom = function(vector, element) {
 
 water = function(x, y, z) {
   molecule = new THREE.Object3D();
-  var startPosition = new THREE.Vector3( 0, 30, 0 );
+  var startPosition = new THREE.Vector3( 0, 0, 0 );
   molecule.add(atom(startPosition, "o"));
-  var secondPosition = new THREE.Vector3( 12, 30, 0 );
+  var secondPosition = new THREE.Vector3( 16, 0, 0 );
   molecule.add(atom(secondPosition, "h"));
   var angle = 104.5;
-  var matrix = new THREE.Matrix4().makeRotationAxis( new THREE.Vector3( 0, 1, 0 ), angle * ( Math.PI / 180 ));
+  var matrix = new THREE.Matrix4().makeRotationAxis( new THREE.Vector3( 0, 0, 1 ), angle * ( Math.PI / 180 ));
   var thirdPosition = secondPosition.applyMatrix4( matrix );
   molecule.add(atom(thirdPosition, "h"));
   molecule.position.set(x, y, z);
@@ -30,7 +32,7 @@ methanum = function(x, y, z) {
   molecule = new THREE.Object3D();
   var startPosition = new THREE.Vector3( 0, 0, 0 );
   molecule.add(atom(startPosition, "c"));
-  var secondPosition = new THREE.Vector3( 0, 12, 0 );
+  var secondPosition = new THREE.Vector3( 0, 22, 0 );
   molecule.add(atom(secondPosition, "h"));
   var angle = 109.5;
   var matrix = new THREE.Matrix4().makeRotationAxis( new THREE.Vector3( 1, 0, 0 ), angle * ( Math.PI / 180 ));
@@ -45,6 +47,23 @@ methanum = function(x, y, z) {
   scene.add( molecule );
 }
 
-water(0,0,0);
-water(30,60,0);
-methanum(-60,60,0);
+ammonia = function(x, y, z) {
+  molecule = new THREE.Object3D();
+  var startPosition = new THREE.Vector3( 0, 0, 0 );
+  molecule.add(atom(startPosition, "n"));
+  var secondPosition = new THREE.Vector3( 0, 18, 0 );
+  molecule.add(atom(secondPosition, "h"));
+  var angle = 107.5;
+  var matrix = new THREE.Matrix4().makeRotationAxis( new THREE.Vector3( 1, 0, 0 ), angle * ( Math.PI / 180 ));
+  var thirdPosition = secondPosition.applyMatrix4( matrix );
+  molecule.add(atom(thirdPosition, "h"));
+  var matrix = new THREE.Matrix4().makeRotationAxis( new THREE.Vector3( 0, 1, 0 ), angle * ( Math.PI / 180 ));
+  var thirdPosition = secondPosition.applyMatrix4( matrix );
+  molecule.add(atom(thirdPosition, "h"));
+  molecule.position.set(x, y, z);
+  scene.add( molecule );
+}
+
+water(-80,40,0);
+methanum(0,40,0);
+ammonia(80,40,0);
